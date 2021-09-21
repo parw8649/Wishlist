988458f257b0e6db73d163373df74e5c48217739
parw8649 <cparwatkar@csumb.edu>
2021-09-21 09:16:27 +0530

    Initial Spring boot application with default configurations

---
+++ src/main/java/com/wishlist/cst438project2/Cst438Project2Application.java
@@ -0,0 +1,12 @@
+package com.wishlist.cst438project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Project2Application.class, args);
+	}
+}
---
+++ src/main/java/com/wishlist/cst438project2/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.wishlist.cst438project2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cst438Project2Application.class);
+	}
+}
