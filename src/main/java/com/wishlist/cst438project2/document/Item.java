cb89da5adc1ed573af8e1cb729dfbdf58e40f3bd
Barbara Kondo <bkondo@csumb.edu>
2021-09-26 14:57:27 -0700

    added Item class with vars: name, link, description, and imgUrl

---
+++ src/main/java/com/wishlist/cst438project2/document/Item.java
@@ -0,0 +1,46 @@
+package com.wishlist.cst438project2.document;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * represents an item to be referenced by Wishlist Class.
+ * referenced constructor chaining: https://www.geeksforgeeks.org/constructor-chaining-java-examples/
+ * @author Barbara Kondo
+ * @version %I% %G%
+ */
+
+@Data
+@NoArgsConstructor
+public class Item {
+    @DocumentId
+    private String name;
+    private String link;
+    private String description;
+    private String imgUrl;
+
+    public Item(String name, String link, String description, String imgUrl) {
+        this(name, link, description);
+        this.imgUrl = imgUrl;
+    }
+
+    public Item(String name, String link, String description) {
+        this(name, link);
+        this.description = description;
+    }
+
+    public Item(String name, String link) {
+        this(name);
+        this.link = link;
+    }
+
+    /**
+     * Creates an Item with a given String name. Item objects must have an assigned name.
+     * @param name is the name or title of the item
+     */
+    public Item(String name) {
+        this.name = name;
+    }
+    
+}
