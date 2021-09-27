5d9dc17a1fd73135f04e11fc0817dc268db2b4cf
Barbara Kondo <bkondo@csumb.edu>
2021-09-26 17:43:10 -0700

    removed unused imports

--- src/main/java/com/wishlist/cst438project2/service/ItemService.java
+++ src/main/java/com/wishlist/cst438project2/service/ItemService.java
@@ -1,13 +1,10 @@
 package com.wishlist.cst438project2.service;
 
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.v1.FirestoreClient;
 import com.wishlist.cst438project2.document.Item;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ItemService {
     public String createItem(Item item);
-    
+
 }
