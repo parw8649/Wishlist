f363fe9006ee480d693a21adfe79d42059e30c39
Barbara Kondo <bkondo@csumb.edu>
2021-09-26 17:17:07 -0700

    changed ItemService from java Class to Interface

--- src/main/java/com/wishlist/cst438project2/service/ItemService.java
+++ src/main/java/com/wishlist/cst438project2/service/ItemService.java
@@ -1,5 +1,13 @@
 package com.wishlist.cst438project2.service;
 
-public class ItemService {
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.v1.FirestoreClient;
+import com.wishlist.cst438project2.document.Item;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ItemService {
+    public String createItem(Item item);
     
 }
