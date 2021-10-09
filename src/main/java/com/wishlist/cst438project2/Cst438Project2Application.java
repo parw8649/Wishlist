2e6ad69c8d92a600daf18b0c780a1910e4f23f30
parw8649 <cparwatkar@csumb.edu>
2021-10-09 18:15:53 +0530

    Updated Jar configurations for deployment

--- src/main/java/com/wishlist/cst438project2/Cst438Project2Application.java
+++ src/main/java/com/wishlist/cst438project2/Cst438Project2Application.java
@@ -2,9 +2,15 @@ package com.wishlist.cst438project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Cst438Project2Application {
+
+	@GetMapping("/")
+	public String healthCheck() {
+		return "Service is running!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cst438Project2Application.class, args);
