33429e3db82e1e47823a17e564040764a255dc43
parw8649 <cparwatkar@csumb.edu>
2021-10-09 19:30:57 +0530

    Server test

--- src/main/java/com/wishlist/cst438project2/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.wishlist.cst438project2.service;
-
-import com.wishlist.cst438project2.document.Product;
-
-public interface ProductService {
-
-    String saveProduct(Product product);
-}
