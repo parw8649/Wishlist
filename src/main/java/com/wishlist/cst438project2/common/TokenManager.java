7a4030aadb3d6838fe6aa1b8660cf7e72c9f6875
parw8649 <cparwatkar@csumb.edu>
2021-10-16 20:21:01 +0530

    Added getUser method in TokenManager to validate and fetch user based on accessToken

--- src/main/java/com/wishlist/cst438project2/common/TokenManager.java
+++ src/main/java/com/wishlist/cst438project2/common/TokenManager.java
@@ -1,7 +1,10 @@
 package com.wishlist.cst438project2.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wishlist.cst438project2.document.User;
 import com.wishlist.cst438project2.dto.UserTokenDTO;
+import com.wishlist.cst438project2.exception.UnauthorizedException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -41,5 +44,31 @@ public class TokenManager {
         JwtBuilder builder = Jwts.builder().claim("user", userTokenDTO).setId(userTokenDTO.getUserId()).setIssuedAt(now);
 
         return builder.signWith(signatureAlgorithm, signingKey).compact();
+    }
+
+    /**
+     * This method is used to validate accessToken and fetch user details
+     * @param accessToken
+     * @return user info
+     */
+    public UserTokenDTO getUser(String accessToken) {
+
+        Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(Constants.KEY_SECRET_TOKEN))
+                .parseClaimsJws(accessToken).getBody();
+
+        UserTokenDTO userTokenDTO;
+
+        if(claims.containsKey("user")) {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            userTokenDTO = objectMapper.convertValue(claims.get("user"), UserTokenDTO.class);
+
+            //TODO: Need to verify the token against userId from DB
+        } else {
+            log.warn("User token invalid or expired!");
+            throw new UnauthorizedException();
+        }
+
+        return userTokenDTO;
     }
 }
