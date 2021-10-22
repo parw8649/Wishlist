17ab634db7b702d2f1c5c2e5a5e1d896c4da77a4
Chaitanya <78239832+parw8649@users.noreply.github.com>
2021-10-22 16:58:26 +0530

    DTO decriptions

--- src/main/java/com/wishlist/cst438project2/dto/AddItemsWishlistDTO.java
+++ src/main/java/com/wishlist/cst438project2/dto/AddItemsWishlistDTO.java
@@ -5,6 +5,11 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+/**
+ * @author Chaitanya Parwatkar
+ * @version %I% %G%
+ */
+
 @Data
 public class AddItemsWishlistDTO {
 
--- src/main/java/com/wishlist/cst438project2/dto/ChangePasswordDTO.java
+++ src/main/java/com/wishlist/cst438project2/dto/ChangePasswordDTO.java
@@ -2,6 +2,12 @@ package com.wishlist.cst438project2.dto;
 
 import lombok.Data;
 
+/**
+ * Data Transfer Object for change password endpoint in UserController.java
+ * @author Chaitanya Parwatkar
+ * @version %I% %G%
+ */
+
 @Data
 public class ChangePasswordDTO {
 
