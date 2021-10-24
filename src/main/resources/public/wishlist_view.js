dd6513cf7c09e42d160206084a2e3ff9cf5587f3
Noah <noahahmed316@gmail.com>
2021-10-23 17:16:36 -0700

    Total items wishlist page is done

--- src/main/resources/public/wishlist_view.js
+++ src/main/resources/public/wishlist_view.js
@@ -1,17 +1,63 @@
 
-async function delete_item() {
-    alert("Item Deleted");
+//Grabbing username and access token with cookie
+var cookies = document.cookie.split("; ");
+console.log(cookies);
+var username = "";
+var accessToken = "";
+for (c in cookies) {
+    if (cookies[c].startsWith("username")) {
+        let user = cookies[c].split("=")
+        username = user[1];
+    }
+    if (cookies[c].startsWith("accessToken")) {
+        let token = cookies[c].split("=")
+        accessToken = token[1];
+    }
+}
+console.log("Accesstoken: ", accessToken);
+console.log("username: ", username);
+
+async function add_to_my_list(itemName,userId){
+    console.log("item name: ", itemName);
+    console.log("userID: ", userId);
+
+    var data;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        console.log(typeof this.responseText);
+        data = JSON.parse(this.responseText);
+        console.log(data);
+        delete data['itemId'];
+        delete data['userId'];
+        console.log(data);
+        let dto = {
+            "name":data['name'],
+            "link":data['link'],
+            "description":data['description'],
+            "imgUrl":data['imgUrl'],
+            "priority":data['priority'],
+        }
+        dto = JSON.stringify(dto)
+        console.log("DTO: ", dto);
+
+        var xhttp2 = new XMLHttpRequest();
+        let DTOnUser = {"itemDTO": data,"username": username};
+        DTOnUser = JSON.stringify(DTOnUser);
+        xhttp2.open("POST", "http://127.0.0.1:8080/wishlist/items", true);
+        xhttp2.setRequestHeader('accessToken',accessToken);
+        xhttp2.setRequestHeader('Content-Type','application/json');
+        xhttp2.send(DTOnUser);
+        }
+    };
+
+    xhttp.open("GET", "http://127.0.0.1:8080/wishlist/items?item_name="+itemName+"&userId="+userId, true);
+    xhttp.setRequestHeader('accessToken',accessToken);
+
+    xhttp.send();
 }
 
 async function fill_template() {
-			// let url = "127.0.0.1:8080/wishlist/items";
-			// let data = await getData(url);
-			// console.log(data.error);
-			// if (data.error == "There is nothing here"){
-			// 	alert("Name not recognized")
-			// }
-
-
 			var data;
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function() {
@@ -25,74 +71,10 @@ async function fill_template() {
 				}
 			};
 
-			//Grabbing username and access token with cookie
-			let cookies = document.cookie.split("; ");
-			console.log(cookies);
-            let username = "";
-            let accessToken = "";
-            for (c in cookies) {
-                if (cookies[c].startsWith("username")) {
-                    let user = cookies[c].split("=")
-                    username = user[1];
-                }
-                if (cookies[c].startsWith("accessToken")) {
-                    let token = cookies[c].split("=")
-                    accessToken = token[1];
-                }
-            }
-            console.log("USERNAME: ", accessToken);
-            console.log("ACCESSTOKEN: ", username);
-
-
-//			 Need to identify how to send header parameters and body parameters for API through below API call
-//			var header = {loginAccessToken}
-//			var body = {}
-
 			xhttp.open("GET", "http://127.0.0.1:8080/wishlist/items", true);
 			xhttp.setRequestHeader('AccessToken',accessToken);
-			xhttp.setRequestHeader('list',username);
 			xhttp.send();
-
-
-
-//		 let data = [
-//		 		{
-//		 			"name": "Playstation 5",
-//		 			"link": "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149",
-//		 			"description": "For some epic gaming",
-//		 			"imgUrl": "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGxheXN0YXRpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-//		 			"userId": 7,
-//		 			"priority": "high"
-//		 		},
-//		 		{
-//		 			"name": "Xbox Series X",
-//		 			"link": "https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324",
-//		 			"description": "For some epic gaming + Halo",
-//		 			"imgUrl": "https://images.unsplash.com/photo-1605901309584-818e25960a8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8eGJveHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-//		 			"userId": 7,
-//		 			"priority": "high"
-//		 		},
-//		 		{
-//		 			"name": "Black Poplin Long Sleeve Button-Up",
-//		 			"link": "https://www.dapperboi.com/collections/button-ups/products/black-poplin-long-sleeve-button-up",
-//		 			"description": "comfy with good fit. medium please :D",
-//		 			"imgUrl": "https://cdn.shopify.com/s/files/1/0876/1046/products/dapper-boi-shirts-pre-order-campaign-black-poplin-long-sleeve-button-up-30307667378349_555x.png?v=1631752257",
-//		 			"userId": 7,
-//		 			"priority": "med"
-//		 		},
-//		 	]
-//
-//            console.log(data);
-//            var template = Handlebars.compile(document.querySelector("#wishlist_template").innerHTML);
-//            var filled = template(data, { noEscape: true });
-//            document.querySelector("#output").innerHTML = filled;
-        }
-
-		// async function getData(url){
-		// 	let response = await fetch(url);
-		// 	let data     = await response.json();
-		// 	return data;
-		// }
+			}
 fill_template();
 
 var accessToken = "";
